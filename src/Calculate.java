74864afb1eba5ae73f91f3ca1f278f00d91227f5
kritz999 <kritikabdas007@gmail.com>
2025-01-07 11:14:13 +0530

    2.15

---
+++ src/Calculate.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Calculate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 10;
+        int sum;
+        sum = n+20;
+        System.out.println("The sum is"+sum);
+
+
+
+    }
+}
